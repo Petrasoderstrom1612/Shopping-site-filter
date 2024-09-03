b09942140b856dd0ca5a69a2c468828c70c1d4ba
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2024-09-03 17:11:33 +0200

    both finished

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const sortBtn = document.getElementById("sort-btn")
 const container = document.getElementById("container")
+const sortBtn2 = document.getElementById("sort-btn2")
+const container2 = document.getElementById("container2")
 
 sortBtn.addEventListener("click", function(){
 container.classList.toggle("reversed-colors")
+})
+
+sortBtn2.addEventListener("click",function(){
+    container2.classList.toggle("reversed-colors")
 })
